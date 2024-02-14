a9c1a3bb01453bb157630bfe0b41c416fcf2bd72
LogeshM05 <112538088+LogeshM05@users.noreply.github.com>
2024-02-14 10:25:33 +0530

    Update sdkFile.js

--- sdkFile.js
+++ sdkFile.js
@@ -4,9 +4,9 @@ console.log("SDK File loaded");
 
 // '_ga_GFFGBBR7C4=GS1.1.1707826027.1.1.1707826192.0.0.0; path=/; expires=Thu, 12 Feb 2026 12:10:24 GMT; domain=resulak.github.io; '
 
-
+// ; domain=logeshm05.github.io; SameSite=None; Secure
 
 // document.addEventListener("click", () => {
-    document.cookie = 'TestKey=TestValue; path=/; expires=Thu, 12 Feb 2026 12:10:24 GMT; domain=logeshm05.github.io; SameSite=None; Secure'
+    document.cookie = 'TestKey=TestValue; path=/; expires=Thu, 12 Feb 2026 12:10:24 GMT'
     console.log("Cookie Set");
 // })
