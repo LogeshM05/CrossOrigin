f9b8349fcc294d51f7bf9ee99ef23b3c9da2d059
LogeshM05 <112538088+LogeshM05@users.noreply.github.com>
2024-02-14 10:07:13 +0530

    Add files via upload

--- sdkFile.js
+++ sdkFile.js
@@ -2,3 +2,11 @@
 
 console.log("SDK File loaded");
 
+// '_ga_GFFGBBR7C4=GS1.1.1707826027.1.1.1707826192.0.0.0; path=/; expires=Thu, 12 Feb 2026 12:10:24 GMT; domain=resulak.github.io; '
+
+
+
+document.addEventListener("click", () => {
+    document.cookie = 'TestKey=TestValue; path=/; expires=Thu, 12 Feb 2026 12:10:24 GMT; domain=github.io; '
+    console.log("Cookie Set");
+})
