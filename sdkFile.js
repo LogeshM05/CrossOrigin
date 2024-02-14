0e009711b68363a40f1e56fad262c946ab36ebdb
LogeshM05 <112538088+LogeshM05@users.noreply.github.com>
2024-02-14 10:48:34 +0530

    Update sdkFile.js

--- sdkFile.js
+++ sdkFile.js
@@ -2,7 +2,7 @@ function setCookie(name, value, expiration) {
     var date = new Date();
     date.setTime(date.getTime() + (expiration * 1000));
     var expires = "; expires=" + date.toGMTString();
-    document.cookie = name + "=" + value + expires + "; path=/";
+    document.cookie = name + "=" + value + expires + ";domian=logeshm05.github.io;path=/";
 }
 
 // Call the function to set the cookie
