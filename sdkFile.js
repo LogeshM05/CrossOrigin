e02ab6f8e2377aa2c8b819ce938dbe38c33294fb
LogeshM05 <112538088+LogeshM05@users.noreply.github.com>
2024-02-14 10:22:52 +0530

    Update sdkFile.js

--- sdkFile.js
+++ sdkFile.js
@@ -6,7 +6,7 @@ console.log("SDK File loaded");
 
 
 
-document.addEventListener("click", () => {
-    document.cookie = 'TestKey=TestValue; path=/; expires=Thu, 12 Feb 2026 12:10:24 GMT; domain=github.io; SameSite=None; Secure'
+// document.addEventListener("click", () => {
+    document.cookie = 'TestKey=TestValue; path=/; expires=Thu, 12 Feb 2026 12:10:24 GMT; domain=logeshm05.github.io; SameSite=None; Secure'
     console.log("Cookie Set");
-})
+// })
